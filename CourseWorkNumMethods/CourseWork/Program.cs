26f51b28b47e5a4c1abac8f08a3ddd795c1b2f24
Mira <141563756+oduvanchikm@users.noreply.github.com>
2024-12-10 22:41:18 +0300

    Update Program.cs

--- CourseWorkNumMethods/CourseWork/Program.cs
+++ CourseWorkNumMethods/CourseWork/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace CourseWork;
+namespace CourseWork;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        OutputResult.OutputResultForH();
+        Func<double, double>[] functionArray = new Func<double, double>[]
+        {
+            x => 1,
+            x => Math.Log(x),
+            x => 1 / (x * x),
+            x => 1 / x,
+            x => x,
+            x => x * x,
+            x => x * x * x
+        };
+        
+        OutputResult.OutputResultForH2(functionArray);
+        OutputResult.OutputResultForO2(functionArray);
+        OutputResult.OutputResultForH2O(functionArray);
+        OutputResult.OutputResultForOH(functionArray);
+        OutputResult.OutputResultForH(functionArray);
+        OutputResult.OutputResultForO(functionArray);
     }
 }
