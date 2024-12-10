0df5e28da0907ba427f4fe70c54e9abe901e0ed5
Mira <141563756+oduvanchikm@users.noreply.github.com>
2024-12-10 22:40:04 +0300

    new

--- CourseWorkNumMethods/CourseWork/FindCoefficients.cs
+++ CourseWorkNumMethods/CourseWork/FindCoefficients.cs
@@ -14,11 +14,8 @@ public class FindCoefficients
                                + coefficients[6] * Math.Pow(temperatureNew, 3);
     }
 
-    public static double[] FindGCoefficients(double[] coefficients, double[] T)
+    public static double[] FindGCoefficientsH2(double[] coefficients, double[] T)
     {
-        // Console.WriteLine("11111The transformed polynomial coefficients are:");
-        // Console.WriteLine(string.Join(", ", coefficients));
-
         int n = T.Length;
         double[] result = new double[n];
 
@@ -31,7 +28,83 @@ public class FindCoefficients
 
         return result;
     }
+    
+    public static double[] FindGCoefficientsH2O(double[] coefficients, double[] T)
+    {
+        int n = T.Length;
+        double[] result = new double[n];
 
+        for (int i = 0; i < n; i++)
+        {
+            var thermoData = ThermodynamicProperties.GetHValuesForTemperatureH2O(T[i]);
+            double phiValue = FindPhiCoefficients(coefficients, T[i]);
+            result[i] = 917.764 - thermoData - T[i] * phiValue;
+        }
+
+        return result;
+    }
+    
+    public static double[] FindGCoefficientsO2(double[] coefficients, double[] T)
+    {
+        int n = T.Length;
+        double[] result = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            var thermoData = ThermodynamicProperties.GetHValuesForTemperatureO2(T[i]);
+            double phiValue = FindPhiCoefficients(coefficients, T[i]);
+            result[i] = 493.566 - thermoData - T[i] * phiValue;
+        }
+
+        return result;
+    }
+    
+    public static double[] FindGCoefficientsOH(double[] coefficients, double[] T)
+    {
+        int n = T.Length;
+        double[] result = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            var thermoData = ThermodynamicProperties.GetHValuesForTemperatureOH(T[i]);
+            double phiValue = FindPhiCoefficients(coefficients, T[i]);
+            result[i] = 423.720 - thermoData - T[i] * phiValue;
+        }
+
+        return result;
+    }
+    
+    public static double[] FindGCoefficientsH(double[] coefficients, double[] T)
+    {
+        int n = T.Length;
+        double[] result = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            var thermoData = ThermodynamicProperties.GetHValuesForTemperatureH(T[i]);
+            double phiValue = FindPhiCoefficients(coefficients, T[i]);
+            result[i] = -619.6395 - thermoData - T[i] * phiValue;
+        }
+
+        return result;
+    }
+    
+    public static double[] FindGCoefficientsO(double[] coefficients, double[] T)
+    {
+        int n = T.Length;
+        double[] result = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            var thermoData = ThermodynamicProperties.GetHValuesForTemperatureO(T[i]);
+            double phiValue = FindPhiCoefficients(coefficients, T[i]);
+            result[i] = -586.37 - thermoData - T[i] * phiValue;
+        }
+
+        return result;
+    }
+
+    // -dG/dT
     public static double[] FindSCoefficients(double[] G, double[] T)
     {
         if (G.Length != T.Length)
@@ -49,60 +122,44 @@ public class FindCoefficients
         return dGdT;
     }
 
-    // public static double[] FindHCoefficients(double[] G, double[] T)
-    // {
-    //     if (G.Length != T.Length)
-    //     {
-    //         Console.WriteLine("G and T must be the same length");
-    //     }
-    //
-    //     int n = T.Length;
-    //     double[] dG_dT = new double[n];
-    //     for (int i = 0; i < n - 1; i++)
-    //     {
-    //         dG_dT[i] = (G[i + 1] - G[i]) / (T[i + 1] - T[i]);
-    //     }
-    //     
-    //     double[] result = new double[dG_dT.Length];
-    //
-    //     for (int i = 0; i < n - 1; i++)
-    //     {
-    //         result[i] = G[i] - T[i] * dG_dT[i];
-    //     }
-    //     
-    //     return result;
-    // }
-    // g + ts
-    public static double[] FindHCoefficients(double[] G, double[] T, double[] S)
+    // g + ts or second dG/dT
+    public static double[] FindHCoefficients(double[] G, double[] T)
     {
         if (G.Length != T.Length)
         {
             Console.WriteLine("G and T must be the same length");
         }
-        
-        double[] result = new double[T.Length];
-
-        for (int i = 0; i < T.Length; i++)
+    
+        int n = T.Length;
+        double[] dG_dT = new double[n];
+        for (int i = 0; i < n - 1; i++)
         {
-            result[i] = G[i] + T[i] * S[i];
+            dG_dT[i] = (G[i + 1] - G[i]) / (T[i + 1] - T[i]);
+        }
+        
+        double[] result = new double[dG_dT.Length];
+    
+        for (int i = 0; i < n - 1; i++)
+        {
+            result[i] = G[i] - T[i] * dG_dT[i];
         }
         
         return result;
     }
     
+    // dH/dT
     public static double[] FindCpCoefficients(double[] H, double[] T)
     {
-        if (H.Length != T.Length)
-        {
-            Console.WriteLine("H and T must be the same length");
-        }
-        
         int n = T.Length;
         double[] dHdT = new double[n];
-        for (int i = 0; i < n - 1; i++)
+    
+        for (int i = 1; i < n - 1; i++)
         {
-            dHdT[i] = (H[i + 1] - H[i]) / (T[i + 1] - T[i]);
+            dHdT[i] = (H[i + 1] - H[i - 1]) / (T[i + 1] - T[i - 1]);
         }
+    
+        dHdT[0] = (H[1] - H[0]) / (T[1] - T[0]);
+        dHdT[n - 1] = (H[n - 1] - H[n - 2]) / (T[n - 1] - T[n - 2]);
         
         return dHdT;
     }
